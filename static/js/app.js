c5747e33a26b735477de3f5cf893c55a416c832f
LL888 <76023384+lyli888@users.noreply.github.com>
2021-04-18 23:56:58 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ function initialize(){
     var dropdown = d3.select("#selDataset");
     var samples = data.samples;
     var metadata = data.metadata;
-    
+
     //D3 Read In Data
     d3.json("data/samples.json").then(data => {
         var names = data.names;
@@ -21,7 +21,7 @@ function initialize(){
     });
 
 };
-j
+
 //Update Charts
 function updateCharts(sample){
     d3.json("data/samples.json").then(data => {
